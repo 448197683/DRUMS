ebade2ab5881bafba17553ebf50601f48922623b
448197683 <82860436+448197683@users.noreply.github.com>
2021-11-15 18:08:05 +0800

    finish

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const drums = document.querySelectorAll('.drum');
 const drumSound = new Audio();
 
+let handleKey = '';
+
 const clearCSSEffect = () => {
   drums.forEach((drum) => {
     drum.classList.remove('hit');
@@ -8,7 +10,6 @@ const clearCSSEffect = () => {
 };
 
 const stroke = (target) => {
-  console.log(target);
   drumSound.src = `audio/${target}.wav`;
   drumSound.play();
   drumSound.currentTime = 0;
@@ -24,12 +25,31 @@ drums.forEach((drum) => {
 });
 
 const handleKeys = (e) => {
-  console.log(e);
+  if (e.key === `k`) {
+    drumSound.src = `audio/kick.wav`;
+    drumSound.play();
+  } else if (e.key === `s`) {
+    drumSound.src = `audio/snare.wav`;
+    drumSound.play();
+  } else if (e.key === `h`) {
+    drumSound.src = `audio/hh.wav`;
+    drumSound.play();
+  } else if (e.key === `t`) {
+    drumSound.src = `audio/tom1.wav`;
+    drumSound.play();
+  } else if (e.key === `y`) {
+    drumSound.src = `audio/tom2.wav`;
+    drumSound.play();
+  } else if (e.key === `u`) {
+    drumSound.src = `audio/tom3.wav`;
+    drumSound.play();
+  } else if (e.key === `c`) {
+    drumSound.src = `audio/crash.wav`;
+    drumSound.play();
+  } else if (e.key === `r`) {
+    drumSound.src = `audio/ride.wav`;
+    drumSound.play();
+  }
 };
 
 document.addEventListener('keypress', handleKeys);
-
-/* k -> kick
-s -> snare
-h -> hh
- */
